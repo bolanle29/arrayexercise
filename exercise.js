63cc64ca9c1868db1ba91e5df848efc084b5d31c
Bolanle Ayodele <royaleunice29@gmail.com>
2021-08-30 06:52:20 +0100

    first commit

---
+++ exercise.js
@@ -0,0 +1,39 @@
+// using loop,iterate through this array and console.log all of the people
+let people=['Greg', 'Mary', 'Devon', 'James'];
+
+// Question one
+// Write command to remove "greg" from the array
+
+// for(let i=0;i<people.length;i++){
+//     console.log("people")
+// }
+
+
+// Question two
+// // Write command to remove "james" from the array
+
+// people.shift
+//     console.log(people)
+// }
+
+
+// Question three
+
+// people.pop()
+// console.log(people)
+
+
+// Question four
+// Write a command to add "matt' to the front of the array
+
+// people.splice(1,0, 'Matt')
+// console.log(people)
+
+
+
+// Question 5
+// Write a command to add your name to the end of the array
+people.splice(3,1 ,'bolanle')
+console.log(people)
+
+
